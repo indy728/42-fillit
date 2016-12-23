cea87f1b222186cd205171acb339265d19c6bda8
Kyle MURRAY <kmurray@e1z2r7p17.42.us.org>
2016-12-22 19:31:05 -0800

    update

---
+++ gitfillit/src/ft_is_tetro.c
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_is_tetro.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kmurray <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/12/17 00:17:38 by kmurray           #+#    #+#             */
+/*   Updated: 2016/12/21 14:39:58 by kmurray          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fillit.h"
+
+static int	tetro_next(char const *tetro, int i)
+{
+	if (tetro[i + 1] == '#')
+		return (1);
+	return (0);
+}
+
+static int	tetro_next_line(char const *tetro, int i)
+{
+	if (tetro[i + 5] == '#')
+		return (1);
+	return (0);
+}
+
+static int	tetro_prev(char const *tetro, int i)
+{
+	if (tetro[i - 1] == '#')
+		return (1);
+	return (0);
+}
+static int	tetro_prev_line(char const *tetro, int i)
+{
+	if (tetro[i - 5] == '#')
+		return (1);
+	return (0);
+}
+
+int	ft_is_tetro(char const *tetro)
+{
+	int	count;
+	int	i;
+
+	count = 0;
+	i = 0;
+	while (tetro[i])
+	{
+		if (tetro[i] == '#' && i == 0)
+			count = count + tetro_next(tetro, i) + tetro_next_line(tetro, i);
+		else if (tetro[i] == '#' && i < 5)
+			count = count + tetro_next(tetro, i) + tetro_next_line(tetro, i)
+				+ tetro_prev(tetro, i);
+		else if (tetro[i] == '#' && i == 18)
+			count = count + tetro_prev(tetro, i) + tetro_prev_line(tetro, i);
+		else if (tetro[i] == '#' && i > 14)
+			count = count + tetro_next(tetro, i) + tetro_prev_line(tetro, i)
+				+ tetro_prev(tetro, i);
+		else if (tetro[i] == '#')
+			count = count + tetro_next(tetro, i) + tetro_prev_line(tetro, i)
+				+ tetro_prev(tetro, i) + tetro_next_line(tetro, i);
+		i++;
+	}
+	if (count >= 6)
+		return (1);
+	return (0);
+}
